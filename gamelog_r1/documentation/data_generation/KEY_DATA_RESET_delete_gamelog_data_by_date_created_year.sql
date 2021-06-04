996baa0c0d9b42a11493cde5621e2895ee98b9c1
John Hart <jhart.naples@gmail.com>
2021-06-04 13:44:08 -0400

    updated location of data files

---
+++ gamelog_r1/documentation/data_generation/KEY_DATA_RESET_delete_gamelog_data_by_date_created_year.sql
@@ -0,0 +1,27 @@
+use gamelog;
+
+-- {MySQL - How to temporarily disable a foreign keyconstraint?|https://tableplus.com/blog/2018/08/mysql-how-to-temporarily-disable-foreign-key-constraints.html]]
+
+SET FOREIGN_KEY_CHECKS=0;
+
+DELETE from division where LEFT(date_created,4)  = '2021';
+DELETE from franchise where LEFT(date_created,4)  = '2021';
+DELETE from weekday where LEFT(date_created,4)  = '2021';
+DELETE from gametype where LEFT(date_created,4)  = '2021';
+DELETE from league where LEFT(date_created,4)  = '2021';
+DELETE from park where LEFT(date_created,4)  = '2021';
+DELETE from season where LEFT(date_created,4)  = '2021';
+DELETE from location where LEFT(date_created,4)  = '2021';
+DELETE from ump_gamelog where LEFT(date_created,4)  = '2021';
+DELETE from tm_gameline_bat where LEFT(date_created,4)  = '2021';
+DELETE from tm_gameline_defense where LEFT(date_created,4)  = '2021';
+DELETE from tm_gameline_pitch where LEFT(date_created,4)  = '2021';
+DELETE from tm_gamelog where LEFT(date_created,4)  = '2021';
+DELETE from lineitem_order where LEFT(date_created,4)  = '2021';
+DELETE from lineitem_pos where LEFT(date_created,4)  = '2021';
+DELETE from team where LEFT(date_created,4)  = '2021';
+DELETE from person where LEFT(date_created,4)  = '2021';
+DELETE from gamelog_awards where LEFT(date_created,4)  = '2021';
+DELETE from gamelog where LEFT(date_created,4)  = '2021';
+
+SET FOREIGN_KEY_CHECKS=1;
